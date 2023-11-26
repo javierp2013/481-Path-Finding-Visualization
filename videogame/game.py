125b8737009e2f0fa92f50e4c6ade9ecf10e9207
Hoovier <javierp2013@csu.fullerton.edu>
2023-11-25 18:54:04 -0800

    Added heuristic, Astar, and some QOL

--- videogame/game.py
+++ videogame/game.py
@@ -4,13 +4,12 @@ import os
 import warnings
 import pygame
 import math
-
 from videogame import rgbcolors
-
+from queue import PriorityQueue
 # Following basic format for boilerplate code in CPSC 386
 # TODO: Maybe move these functions into the VideoGame class?
 # Create the board and populate arrays with designated Node objects
-# Strategy for creating grid referenced here: http://programarcadegames.com/index.php?lang=en&chapter=array_backed_grids 
+# Strategy for creating grid/board referenced here: http://programarcadegames.com/index.php?lang=en&chapter=array_backed_grids 
 def create_board(rows, width):
         board = []
         #determine the nearest whole number for space between nodes
@@ -35,13 +34,76 @@ def draw_board(rows, width, window):
             pygame.draw.line(window, rgbcolors.blueviolet, (col * space, 0), (col * space, width))
 
 # Draw each node to screen!
-def draw(window, grid, rows, width):
+def draw(window, board, rows, width):
     window.fill(rgbcolors.ghost_white)
-    for row in grid:
+    for row in board:
         for node in row:
             node.draw(window)
     draw_board(rows, width, window)
     pygame.display.update()
+
+def getMouse(mousePos, rows, width):
+    yPos, xPos = mousePos
+    space = math.floor(width / rows)
+    return math.floor(yPos / space), math.floor(xPos / space)
+
+def manhattanDistance(p1, p2):
+	x1, y1 = p1
+	x2, y2 = p2
+	return abs(x1 - x2) + abs(y1 - y2)
+
+
+def colorFinalPath(previous, currNode, draw):
+	while currNode in previous:
+		currNode = previous[currNode]
+		currNode.definePath()
+		draw()
+
+def astarRun(draw, board, start, end):
+	count = 0
+	frontier = PriorityQueue()
+	frontier.put((0, count, start))
+	previous = {}
+	pathCost = {node: float("inf") for row in board for node in row}
+	pathCost[start] = 0
+	heuristic = {node: float("inf") for row in board for node in row}
+	heuristic[start] = manhattanDistance(start.getPos(), end.getPos())
+    #keep track of items in priority queue
+	frontier_hash = {start}
+
+	while not frontier.empty():
+        #2 gets the node since its the 3rd item in that set, preceded by heuristic and count
+		currNode = frontier.get()[2]
+		frontier_hash.remove(currNode)
+        #we're done and can now draw the final path
+		if currNode == end:
+			colorFinalPath(previous, end, draw)
+			end.defineEnd()
+			return True
+          
+		for neighbor in currNode.neighbors:
+            #since we are moving only one node ahead, we can add 1 to pathcost.
+			temp_pathCost = pathCost[currNode] + 1
+            #if this currNode pathcost is smaller than the neighbors, make this the path we choose
+			if temp_pathCost < pathCost[neighbor]:
+				previous[neighbor] = currNode
+				pathCost[neighbor] = temp_pathCost
+				heuristic[neighbor] = temp_pathCost + manhattanDistance(neighbor.getPos(), end.getPos())
+				if neighbor not in frontier_hash:
+					count += 1
+					frontier.put((heuristic[neighbor], count, neighbor))
+					frontier_hash.add(neighbor)
+					neighbor.open()
+		draw()
+		if currNode != start:
+			currNode.close()
+
+	return False
+
+
+
+
+
 
 class VideoGame:
     """Base class for creating PyGame games."""
@@ -66,11 +128,35 @@ class VideoGame:
         """Run the game; the main game loop."""
         row_num = 40
         board = create_board(row_num, width)
+        beginning = None
+        end = None
         while True:
             draw(window, board, row_num, width)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
+                #Left mouse click
+                if pygame.mouse.get_pressed()[0]:
+                    pos = pygame.mouse.get_pos()
+                    row, col = getMouse(pos, row_num, width)
+                    node = board[row][col]
+                    if not beginning:
+                        beginning = node
+                        beginning.defineBeginning()
+                    elif not end and node != beginning:
+                        end = node
+                        end.defineEnd()
+                    elif node != end and node != beginning:
+                        node.defineWall()
+                
+                if event.type == pygame.KEYDOWN:
+                    #only run if the beginning and end are defined.
+                    if event.key == pygame.K_SPACE and beginning and end:
+                        for row in board:
+                            for node in row:
+                                node.defineNeighbors(board)
+                        astarRun(lambda: draw(window, board, row_num, width), board, beginning, end)
+                    
 
 
 
@@ -86,6 +172,47 @@ class Node:
         self.row_total = total
         self.row = row
         self.col = column
+        self.neighbors = []
     
     def draw(self, window):
         pygame.draw.rect(window, self.color, (self.x_pos, self.y_pos, self.width, self.width))
+    
+    
+    def defineBeginning(self):
+        self.color = rgbcolors.azure4
+    def defineEnd(self):
+        self.color = rgbcolors.purple
+    def defineWall(self):
+        self.color = rgbcolors.dark_red
+    def isWall(self):
+        return self.color == rgbcolors.dark_red
+    def definePath(self):
+         self.color = rgbcolors.green4
+    
+    
+        #fills out the neighbors for each node
+    def defineNeighbors(self, board):
+        self.neighbors = []
+        #checks neighbor to the north
+        if self.row > 0 and not board[self.row - 1][self.col].isWall():
+            self.neighbors.append(board[self.row - 1][self.col])
+        #south
+        if self.row < self.row_total - 1 and not board[self.row + 1][self.col].isWall():
+            self.neighbors.append(board[self.row + 1][self.col])
+        #left
+        if self.col > 0 and not board[self.row][self.col - 1].isWall(): # LEFT
+            self.neighbors.append(board[self.row][self.col - 1])
+        #right
+        if self.col < self.row_total - 1 and not board[self.row][self.col + 1].isWall(): # RIGHT
+            self.neighbors.append(board[self.row][self.col + 1])
+    def getPos(self):
+         return self.row, self.col
+    def open(self):
+         self.color = rgbcolors.aquamarine
+    def isOpen(self):
+         return self.color == rgbcolors.aquamarine
+    def close(self):
+         self.color = rgbcolors.black
+    def isClosed(self):
+        return self.color == rgbcolors.black
+
